3bece556948b35ce6c787e103de121abd4ca3091
Rishon Kumar Rahi <rishon.kumar.rahi@gmail.com>
2022-01-02 16:14:22 +0530

    Java Script

---
+++ JavaScript/Functions.js
@@ -0,0 +1,15 @@
+// with paramter
+function showAlert(msg) {
+  alert(msg);
+}
+
+function sum(a, b) {
+  //console.log(a + b);
+  return a + b;
+}
+
+var ans = sum(4, 4, 5);
+console.log(ans);
+
+//calling function
+showAlert("Hello");
